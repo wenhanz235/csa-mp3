8482a64bfffb353b54632786e3ae1ef876b503f5
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-05-22 09:43:11 -0400

    Update Main.java

--- frq solution/q2/Main.java
+++ frq solution/q2/Main.java
@@ -1,1 +1,40 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("2024 Question 2!");
 
+     String info;
+     Scoreboard game = new Scoreboard("Red", "Blue");
+     info = game.getScore(); 
+    System.out.println(info);
+     game.recordPlay(1); 
+     info = game.getScore(); 
+    System.out.println(info);
+     game.recordPlay(0); 
+     info = game.getScore(); 
+     System.out.println(info);
+     info = game.getScore(); 
+     System.out.println(info);
+
+     game.recordPlay(3); 
+     info = game.getScore(); 
+     System.out.println(info);
+
+     game.recordPlay(1); 
+     game.recordPlay(0);
+     info = game.getScore();
+     System.out.println(info);
+
+     game.recordPlay(0); 
+     game.recordPlay(4); 
+     game.recordPlay(0); 
+     info = game.getScore();
+     System.out.println(info);
+
+     Scoreboard match =new Scoreboard("Lions", "Tigers");
+     info = match.getScore();
+     System.out.println(info);
+     info = game.getScore(); 
+     System.out.println(info);
+   
+  }
+}
