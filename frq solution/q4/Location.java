55874d696c19c5f0c558da87e83b475b8696d517
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-05-22 09:48:13 -0400

    Update Location.java

--- frq solution/q4/Location.java
+++ frq solution/q4/Location.java
@@ -1,1 +1,14 @@
-
+public class Location
+{
+private int theRow;
+private int theCol;
+public Location(int r, int c)
+{
+theRow = r;
+theCol = c;
+}
+public int getRow()
+{ return theRow; }
+public int getCol()
+{ return theCol; }
+}
