514861bc6cd007dddde38eee6a19f2679f8e8bcb
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-05-22 09:47:21 -0400

    Update Main.java

--- frq solution/q3/Main.java
+++ frq solution/q3/Main.java
@@ -1,1 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    ArrayList <String> list=new ArrayList<String>();
+    list.add("an");
+    list.add("band");
+    list.add("band");
+    list.add("abandon");
+    WordChecker w= new WordChecker (list);
+    System.out.println(w.isWordChain());
+    System.out.println(w.createList("ba"));
+  }
+}
 
