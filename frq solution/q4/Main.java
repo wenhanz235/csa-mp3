402cf94150512ad34785c054925f82f121414afe
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-05-22 09:48:36 -0400

    Update Main.java

--- frq solution/q4/Main.java
+++ frq solution/q4/Main.java
@@ -1,1 +1,9 @@
-
+class Main {
+  public static void main(String[] args) {
+    int [][] list={{12,30,40,25,5},{11,3,22,15,43},{7,2,9,4,0},{8,33,18,6,1}};
+        GridPath g= new GridPath(list);
+    System.out.println(g.sumPath(1,1));
+   
+  }
+}
+ 
