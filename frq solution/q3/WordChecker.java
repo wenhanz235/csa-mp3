5fa66ae7950fb2ff77753ed1be2ba1b20c3caca5
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-05-22 09:46:53 -0400

    Update WordChecker.java

--- frq solution/q3/WordChecker.java
+++ frq solution/q3/WordChecker.java
@@ -1,1 +1,30 @@
+import java.util.*;
+public class WordChecker{
+  private ArrayList<String> wordlist;
+  public WordChecker(ArrayList<String>list){
+    wordlist=list;
+    
+  }
+  public boolean isWordChain(){
+    for(int i=1;i<wordlist.size();i++){
+      if (wordlist.get(i).indexOf(wordlist.get(i-1))==-1){
+        return false;
+      }
+    }
+    return true;
+    }
+  public ArrayList<String> createList(String target){
+    ArrayList<String> arr= new ArrayList<String>();
+    for (String str: wordlist){
+      if (str.indexOf(target)>=0){
+        if (str.length()==target.length()){
+          arr.add("");
+        }else{
+          arr.add(str.substring(target.length()));
+        }
+      }
+    }
+    return arr;
+  }
+  }
 
