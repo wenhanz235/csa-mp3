556a9aa537f13f5fd8e2059cfc887553fb92afe5
wenhanz235 <159273409+wenhanz235@users.noreply.github.com>
2024-06-06 22:24:33 -0400

    Update Main.java

--- frq solution/q1/Main.java
+++ frq solution/q1/Main.java
@@ -3,6 +3,9 @@ class Main {
     Feeder f= new Feeder(100);
     f.simulateOneDay(10);
     System.out.println(f.simulateManyDay(10,5));
+    Feeder m= new Feeder(200);
+    m.simulateOneDay(20);
+    System.out.println(m.simulateManyDay(20,8));
   }
 }
 
